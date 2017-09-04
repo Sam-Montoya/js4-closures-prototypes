a52b11e31715dd5a80033df5f216352829cc9fd5
Brettly <brettly@Brettlys-MacBook-Pro.local>
2017-09-04 12:09:07 -0700

    Updated to repo.

--- constructors.js
+++ constructors.js
@@ -9,7 +9,10 @@
 // Code here
 
 
-
+function CarFactory ( make, model ) {
+  this.make = make;
+  this.model = model;
+}
 
 
 // ======================
@@ -19,17 +22,17 @@
 // Invoke the constructor function below and pass in 'Bob', 'bob@gmail.com', and '01-02-98' as arguments. Assign the result of the invocation to a variable called bob.
 
 
-function Employee(name, email, hireDate) {
-    this.name = name;
-    this.email = email;
-    this.hireDate = hireDate;
+function Employee ( name, email, hireDate ) {
+  this.name = name;
+  this.email = email;
+  this.hireDate = hireDate;
 }
 
 // Code here
 
 
 
-
+var bob = new Employee( 'Bob', 'bob@gmail.com', '01-02-98' );
 
 
 
@@ -42,18 +45,28 @@ function Employee(name, email, hireDate) {
 
 // Code here
 
+function Car ( model, type, year ) {
+  this.model = model;
+  this.type = type;
+  this.year = year;
+  this.move = 0;
+
+  this.moveCar = function () {
+    return this.move += 10;
+  };
+
+}
 
 
+//Function Invocations Here
 
-  //Function Invocations Here
+var prius = new Car( 'Toyota', 'Prius', 2011 );
+var mustang = new Car( 'Ford', 'Mustang', 2013 );
 
-  var prius = new Car('Toyota', 'Prius', 2011);
-  var mustang = new Car('Ford', 'Mustang', 2013);
-  
-  //Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
-  
-  prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
-  mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
+//Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
+
+prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
+mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
 
 
